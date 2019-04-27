747793f51a7724c1f7eb31c2f81a47e666d3963e
dex6ter2 <September!983>
2019-04-27 16:00:11 +0300

    Placed register, modified Login with new Mapping for registration, registration cant be accessed

--- src/main/java/project/marius/siit/mytrip/MyTrip/Controller/LoginController.java
+++ src/main/java/project/marius/siit/mytrip/MyTrip/Controller/LoginController.java
@@ -2,16 +2,20 @@ package project.marius.siit.mytrip.MyTrip.Controller;
 
         import org.springframework.stereotype.Controller;
         import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.RequestMapping;
         import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(path = "/login")
 public class LoginController {
 
-    @GetMapping(value="")
+    @GetMapping("/login")
     public ModelAndView displayStartPage(){
         ModelAndView mv = new ModelAndView("login");
+        return mv;
+    }
+
+    @GetMapping("/registration")
+    public ModelAndView displayRegistrationPage() {
+        ModelAndView mv = new ModelAndView("register");
         return mv;
     }
 }
